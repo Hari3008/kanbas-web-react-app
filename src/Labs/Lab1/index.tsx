b11afe50c6d354cc478dca655aa59333bc297f8f
Hari3008 <hari30082001@gmail.com>
2024-09-14 13:37:08 -0400

    update img

--- src/Labs/Lab1/index.tsx
+++ src/Labs/Lab1/index.tsx
@@ -157,6 +157,7 @@ export default function Lab1() {
             id="wd-starship"
             width="400px"
             src="https://www.staradvertiser.com/wp-content/uploads/2021/08/web1_Starship-gap2.jpg"
+            alt=""
           />
           <br />
           Loading a local image:
