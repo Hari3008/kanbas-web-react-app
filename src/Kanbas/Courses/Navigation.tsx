f5460f7a20455ed159e1885196f0a08f0cc65299
Hari3008 <hari30082001@gmail.com>
2024-10-20 20:54:27 -0400

    courses section responsiveness

--- src/Kanbas/Courses/Navigation.tsx
+++ src/Kanbas/Courses/Navigation.tsx
@@ -1,69 +1,73 @@
-import { Link } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 export default function CoursesNavigation() {
   return (
     <div id="wd-courses-navigation" className="wd list-group fs-5 rounded-0">
-      <Link
+      <NavLink
         to="/Kanbas/Courses/1234/Home"
         id="wd-course-home-link"
-        className="list-group-item active border border-0"
+        className={({ isActive }) =>
+          isActive
+            ? "list-group-item border border-0 active text-black"
+            : "list-group-item border text-danger border-0"
+        }
       >
         {" "}
         Home{" "}
-      </Link>
+      </NavLink>
       
-      <Link
+      <NavLink
         to="/Kanbas/Courses/1234/Modules"
         id="wd-course-modules-link"
         className="list-group-item text-danger border border-0"
       >
         {" "}
         Modules{" "}
-      </Link>
+      </NavLink>
       
-      <Link
+      <NavLink
         to="/Kanbas/Courses/1234/Piazza"
         id="wd-course-piazza-link"
         className="list-group-item text-danger border border-0"
       >
         {" "}
         Piazza{" "}
-      </Link>
+      </NavLink>
       
-      <Link
+      <NavLink
         to="/Kanbas/Courses/1234/Zoom"
         id="wd-course-zoom-link"
         className="list-group-item text-danger border border-0"
       >
         {" "}
         Zoom{" "}
-      </Link>
+      </NavLink>
       
-      <Link
+      <NavLink
         to="/Kanbas/Courses/1234/Assignments"
         id="wd-course-quizzes-link"
         className="list-group-item text-danger border border-0"
       >
         {" "}
         Assignments{" "}
-      </Link>
+      </NavLink>
       
-      <Link
+      <NavLink
         to="/Kanbas/Courses/1234/Quizzes"
         id="wd-course-assignments-link"
         className="list-group-item text-danger border border-0"
       >
         {" "}
         Quizzes{" "}
-      </Link>
+      </NavLink>
       
-      <Link
+      <NavLink
         to="/Kanbas/Courses/1234/People"
         id="wd-course-people-link"
         className="list-group-item text-danger border border-0"
       >
         {" "}
         People{" "}
-      </Link>
+      </NavLink>
       
     </div>
   );
